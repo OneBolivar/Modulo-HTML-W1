d3e9de6a7f00ef79041231e7f147bc3883e3829a
One <jebolivar33@gmail.com>
2026-05-06 15:18:03 -0500

    Prueba con TL David

--- Module JS/api.js
+++ Module JS/api.js
@@ -1,74 +1,64 @@
-// URL endpoint 
-const url = "https://rickandmortyapi.com/api/character";
-const page_number = 1
-const url_api = `${url}?page=${page_number}`
-
+const url_api = "https://rickandmortyapi.com/api/character"
 
 /**
- * requestData
  * 
- * Send request to Endpoint
- * @param {string} url_api
- *
+ * @param {string} url_api 
  */
 async function requestData(url_api) {
     const response = await fetch(url_api);
     let data = await response.json();
-    getElementButton(document, 'set', data.info)
     renderHtml(data);
+    setAttributeCustom(data.info);
 }
 
 /**
- * loadMore
  * 
- * Call <@&1440671698327175178>unction getElementButton 
+ * @param {object} data 
  */
-function loadMore() {
-    getElementButton(document, 'get')
-}
-
-/**
- * getElementButton
- * 
- * @param {object} elementButton
- * @param {object} button
- * @param {string} operation
- */
-function getElementButton(elementButton, operation = 'get', info = null) {
-    const button = elementButton.getElementById("loadMore");
-    if(operation == 'get'){
-        const next = button.getAttribute("data-next");
-        if(next == "" || next == null){
-            console.log("No hay url")
-        }else {
-            requestData(next);
-        }
-    }else {
-        button.setAttribute("data-next", (info.next == null) ? '' : info.next)
-        button.setAttribute("data-prev", (info.prev == null) ? '' : info.prev)
+function renderHtml(data) {
+    let lista = document.getElementById("character")
+    lista.innerHTML = ""
+    for (let index = 0; index < data.results.length; index++) {
+        let name = data.results[index].name
+        let image = data.results[index].image
+        let gender = data.results[index].gender
+        lista.innerHTML += `<li>
+            <img src="${image}">
+            <h2>${name}</h2>
+            <h3>${gender}</h3>
+            </li>`
     }
-   
 }
 
-/**
- * renderHtml
- * @param {object} element
- * @param {object} data
- */
-function renderHtml(data){
-    let element = document.getElementById("character");
-    let resultCount = data.results.length;
-    
-    for (let index = 0; index < resultCount; index++) {
-        let character = data.results[index];
-
-        element.innerHTML += `<li>
-            <img src="${character.image}" alt="${character.name}">
-            <h2>${character.name}</h2>
-            <span>${character.gender}</span>
-        </li>`; 
+function loadNext(){
+    const infoNextPrev = getAttributeCustom();
+    if(infoNextPrev.next != "") {
+        requestData(infoNextPrev.next);
     }
-    
 }
 
-const response = requestData(url_api);
+function loadPrev(){
+    const infoNextPrev = getAttributeCustom();
+    if(infoNextPrev.prev != "") {
+        requestData(infoNextPrev.prev);
+    }
+}
+
+function setAttributeCustom(info){
+    const buttonNext = document.getElementById("loadNextId");
+    const buttonPrev = document.getElementById("loadPrevId");
+    buttonNext.setAttribute("data-next", (info.next != null ) ? info.next : "")
+    buttonPrev.setAttribute("data-prev", (info.prev != null) ? info.prev : "")
+}
+
+
+function getAttributeCustom(){
+    const buttonNext = document.getElementById("loadNextId");
+    const buttonPrev = document.getElementById("loadPrevId");
+    const next = buttonNext.getAttribute("data-next")
+    const prev = buttonPrev.getAttribute("data-prev")
+    const returnInfo = {next, prev}
+    return returnInfo
+}
+
+const response = requestData(url_api)
